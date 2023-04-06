7db1eb7d802721bbed23c28006dae428ccf89cfd
STEEL-PROJECTS0\ccoley <ccoley@steelprojects.com>
2023-04-06 14:53:29 +0200

    first commit

---
+++ Models/SchoolApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    public class SchoolApiContext : DbContext
+    {
+        public SchoolApiContext(DbContextOptions<SchoolApiContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+     
+    
+    }
+}
